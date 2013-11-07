a08214145b652bfc7bfd9bc0f4f91747c8683031
Yoshiyuki TANIGUCHI <b-yostan@microsoft.com>
2013-11-07 14:39:13 +0900

    insert open workbook method

--- Xlsx2DMLandINSERT/Program.cs
+++ Xlsx2DMLandINSERT/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
 namespace Xlsx2DMLandINSERT
 {
     class Program
@@ -11,5 +15,17 @@ namespace Xlsx2DMLandINSERT
         static void Main(string[] args)
         {
         }
+        HSSFWorkbook hssfworkbook;
+
+        void InitializeWorkbook(string path)
+        {
+            //read the template via FileStream, it is suggested to use FileAccess.Read to prevent file lock.
+            //book1.xls is an Excel-2007-generated file, so some new unknown BIFF records are added. 
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                hssfworkbook = new HSSFWorkbook(file);
+            }
+        }
+
     }
 }
